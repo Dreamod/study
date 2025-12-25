a47007a838ced3956a568c98e8185159bbfa8325
Dreamod <dreamod1@gmail.com>
2025-12-25 19:04:02 +0300

    task 4.15.4503

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	go printMessage()
+	time.Sleep(1 * time.Second)
+}
+
+func printMessage() {
+	fmt.Println("Hello from goroutine!")
 }
