5f6accf9c7d239411751914540e0ed606171a0b4
Dreamod <dreamod1@gmail.com>
2026-01-09 23:02:41 +0300

    init Concurrency

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	go printMessage()
-	time.Sleep(1 * time.Second)
-}
 
-func printMessage() {
-	fmt.Println("Hello from goroutine!")
 }
