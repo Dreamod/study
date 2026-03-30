ab7fda404d07f1bafec9a644c097f0d06d261cf7
Dreamod <dreamod1@gmail.com>
2026-03-30 08:45:23 +0300

    ДЗ модель продукта + миграции, фикс main.go

--- 4-order-api/cmd/main.go
+++ 4-order-api/cmd/main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"go/study/4-order-api/configs"
+	"go/study/4-order-api/pkg/db"
 )
 
 func main() {
 	conf := configs.LoadConfig()
 	_ = db.NewDb(conf)
-	router := http.NewServeMux()
-	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
-		Config: conf,
-	})
-	verify.NewVerifyHandler(router, verify.VerifyHandlerDeps{
-		Config: conf,
-	})
-
-	server := http.Server{
-		Addr:    ":8081",
-		Handler: router,
-	}
-
-	fmt.Println("Server is running on port 8081")
-	server.ListenAndServe()
 }
